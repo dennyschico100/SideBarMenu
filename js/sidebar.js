42f4d04ccfa021244f41af595f779aeea082da95
dennyschico1 <dennyschico2@gmail.com>
2020-06-01 11:33:23 +0200

    preloader added

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,4 @@
 
-
-
 const toggleBtn=document.querySelector(".sidebar-toggle");
 const closeBtn=document.querySelector(".close-btn");
 const sidebar=document.querySelector(".sidebar");
@@ -11,7 +9,6 @@ toggleBtn.addEventListener("click",function(){
 
     }else{
         sidebar.classList.add("show-sidebar");
-
 
     }
 
@@ -26,7 +23,41 @@ closeBtn.addEventListener('click',function(){
 
 window.addEventListener("DOMContentLoaded",function(){
     let ancho=window.screen.width;
+    let body=this.document.getElementsByTagName("body")[0];
+    //body.style.opacity="1";
+
+    
     if(ancho <= 800 ){
 
     }
+})
+
+
+let body=this.document.getElementsByTagName("body")[0];
+let header=this.document.getElementsByTagName("header")[0];
+
+body.style.opacity="1.0";
+
+let div=this.document.createElement('div');
+div.classList.add("loading");
+
+let span=this.document.createElement("span");
+span.textContent="Onichan :V  ...";
+span.style.color="red";
+div.append(span);
+body.append(div);
+header.style.opacity="0.0";
+
+setTimeout( function(){
+
+div.classList.remove("loading");
+div.removeChild(span);
+header.style.opacity="1.0";
+}  ,3000);
+
+
+window.addEventListener('load',function(){
+   
+
+
 })
